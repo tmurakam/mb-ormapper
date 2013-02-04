cf26e5fb6df154fefebee806dcfe3c4289cdf95e
Takuya Murakami <tmurakam@tmurakam.org>
2013-02-05 01:44:28 +0900

    bug fix: iOS

--- orschema.rb
+++ orschema.rb
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 =begin
-  O/R Mapper library for Android
+  mb-ormapper : O/R Mapper library for iOS/Android
+  https://github.com/tmurakam/mb-ormapper
 
-  Copyright (c) 2010-2011, Takuya Murakami. All rights reserved.
+  Copyright (c) 2010-2013, Takuya Murakami. All rights reserved.
 
   Redistribution and use in source and binary forms, with or without
   modification, are permitted provided that the following conditions are
@@ -12,14 +13,6 @@
 
   1. Redistributions of source code must retain the above copyright notice,
   this list of conditions and the following disclaimer. 
-
-  2. Redistributions in binary form must reproduce the above copyright
-  notice, this list of conditions and the following disclaimer in the
-  documentation and/or other materials provided with the distribution. 
-
-  3. Neither the name of the project nor the names of its contributors
-  may be used to endorse or promote products derived from this software
-  without specific prior written permission. 
 
   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
