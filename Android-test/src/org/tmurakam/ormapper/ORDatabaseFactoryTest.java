2e9e524e0b926fc80c8e1a1cc2d47356fc376182
Takuya Murakami <tmurakam@tmurakam.org>
2013-03-19 20:50:40 +0900

    add tests

--- Android-test/src/org/tmurakam/ormapper/ORDatabaseFactoryTest.java
+++ Android-test/src/org/tmurakam/ormapper/ORDatabaseFactoryTest.java
@@ -14,10 +14,29 @@ public class ORDatabaseFactoryTest extends AndroidTestCase {
         mFactory = new ORDatabaseFactory();
     }
     
+    /**
+     * 正常初期化テスト。
+     */
     public void testInitialize() {
         mFactory.initialize(mContext, "test.db");
         assertSame(mContext.getApplicationContext(), mFactory.mApplicationContext);
         assertEquals("test.db", mFactory.mDatabaseName);
     }
+    
+    /**
+     * 初期化 : databaseName が null のとき初期化されないこと。
+     */
+    public void testInitializeNullDatabaseName() {
+        mFactory.initialize(mContext, null);
+        assertNull(mFactory.mDatabaseName);
+    }
 
+    /**
+     * setDatabaseName : 上書きすること。
+     */
+    public void testSetDatabaseName() {
+        mFactory.initialize(mContext, "test.db");
+        mFactory.setDatabaseName("test2.db");
+        assertEquals("test2.db", mFactory.mDatabaseName);
+    }
 }
---
+++ Android-test/src/org/tmurakam/ormapper/ORDatabaseTest.java
@@ -0,0 +1,11 @@
+package org.tmurakam.ormapper;
+
+import android.test.AndroidTestCase;
+
+public class ORDatabaseTest extends AndroidTestCase {
+    /**
+     * 初期化テスト
+     */
+    public void testInitialize() {
+    }
+}
